ddbdcd9ffff0faeaf00a37e1bb9c5d0745d2d4ee
Dhruwang <dhruwangjariwala18@gmail.com>
2022-11-08 11:00:52 +0530

    added fetch api feature

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
+
 export default class News extends Component {
+
+
+  articles = []
+  constructor() {
+    super();
+    this.state = {
+      article: this.articles,
+      loading: false
+    }
+  }
+    async componentDidMount(){
+      console.log("cdm");
+      let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=a6f544932ea2440cb5979ed8049f729e";
+      let data = await fetch(url)
+      let parsedData = await data.json();
+      this.setState({article: parsedData.articles})
+
+    }
+
   render() {
     return (
-        <>
-      <div>this is news</div>
-      <NewsItem />
-      </>
+      <div className='container'>
+        <h1 className='my-4'>WhatsNew - Trending news</h1>
+        <div className='row'>
+          {this.state.article.map((element) => {
+            console.log(element.title)
+            return <div className = 'col-md-4' key={element.url} >
+            < NewsItem key = {element.url} title={element.title.slice(0,40)} description={element.description.slice(0,90)} imageUrl={element.urlToImage} newsUrl={element.url} />
+        </div>
+          })}
+      </div>
+
+
+      </div >
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let {title,description,imageUrl,newsUrl} = this.props;
     return (
-      <div>This is news item</div>
+      <div className="card" style={{width: "30 rem"}}>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href={newsUrl} className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     )
   }
 }
